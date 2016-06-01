d1ecccd587b32ba49771f96e16118db5bfce1052
haqasad <haq.asad000@gmail.com>
2016-06-01 18:20:45 -0400

    Creating CSharpPractice Solution

---
+++ CSharpPractice/Practice1/DataTypeandSystemMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    public class DataTypeandSystemMethods
+    {
+        public void InputFile()
+        {
+            string s = ".NET";
+            Console.WriteLine(s.ToLower());
+            Console.WriteLine(s.Length);
+            Console.ReadLine();
+        }        
+    }
+}
---
+++ CSharpPractice/Practice1/HelloWorldApp.cs
@@ -0,0 +1,27 @@
+﻿// Including the class 'System'. Methods used in the Main method belongs to the System class
+
+using System;
+
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+// The concept of namespace is similar to packages in java. It can hold multiple class files.
+// Java accesses jar file (or packages are compressed as jar file)
+// C# accesses dll file (or namespaces are compressed as dll file)
+// Each project can be considered as a package or namespace. This concept is called Modular Programming
+// Here under Practice1, HelloWorldApp is a class. Also multiple classes (or items like XML) can be added under Practice1
+namespace Practice1 
+{
+    class HelloWorldApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World"); // Prints string
+            Console.Read(); // Holds the output window
+            DataTypeandSystemMethods TypeWritter = new DataTypeandSystemMethods();
+            TypeWritter.InputFile();
+        }
+    }
+}
