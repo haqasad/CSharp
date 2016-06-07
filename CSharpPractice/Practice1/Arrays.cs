1e8bd7421b527de915cba347dbdfe88fb560c94e
Asadul Haque <Asadul Haque>
2016-06-07 00:38:06 -0400

    Modified Arrays.cs

--- CSharpPractice/Practice1/Arrays.cs
+++ CSharpPractice/Practice1/Arrays.cs
@@ -8,11 +8,26 @@ namespace Practice1
 {
     class DeclareArraysSample
     {
-        void array()
+        int v;
+        public DeclareArraysSample() { }
+        public DeclareArraysSample(int a)
+        {
+            if
+        }
+        public void arraySample()
         {
             int[] numbers = new int[5];
             string[,] names = new string[5, 4];
-            byte[][] scores = new byte[5][];
+            byte[][] scores = new byte[v][];
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+                scores[i] = new byte[i + 3];
+            }
+            for (int i = 0; i < scores.Length; i++)
+            {
+                Console.WriteLine("Length of row {0} is {1}", i, scores[i].Length);
+            }
         }
     }
 }
