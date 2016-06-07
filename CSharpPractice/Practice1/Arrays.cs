fa4d0bd7eb4d3dd8cd7e549155005b6f250be4e0
Asadul Haque <Asadul Haque>
2016-06-06 23:58:21 -0400

    Added changes in Arrays.cs

---
+++ CSharpPractice/Practice1/Arrays.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    class DeclareArraysSample
+    {
+        void array()
+        {
+            int[] numbers = new int[5];
+            string[,] names = new string[5, 4];
+            byte[][] scores = new byte[5][];
+        }
+    }
+}
